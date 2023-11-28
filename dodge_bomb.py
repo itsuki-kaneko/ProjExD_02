0254a0b92a6fe32515b055e2d07457f238881148
Kaneko_I <c0a22045a5@edu.teu.ac.jp>
2023-11-28 14:07:19 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,7 @@
 import sys
+import random
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -8,9 +10,15 @@ WIDTH, HEIGHT = 1600, 900
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")  # 背景画像
+    kk_img = pg.image.load("ex02/fig/3.png")  # こうかとんその3
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0) 
+    bomb_img = pg.Surface((20, 20))  # 練習1 爆弾surfaceを作る
+    pg.draw.circle(bomb_img, (255, 0, 0), (10, 10), 10)
+    bomb_rect = bomb_img.get_rect()
+    bomb_rect.centerx = random.randint(0, WIDTH)
+    bomb_rect.centery = random.randint(0, HEIGHT)
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +28,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb_img, bomb_rect)
         pg.display.update()
         tmr += 1
         clock.tick(10)
@@ -30,3 +39,17 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
+
+# docstringの書き方
+# def hage(fuga, piyo):
+#     """
+#     ここに説明
+#     ここに説明
+#     """
+#     pass
+
+
+# トップレベルの空行は2行
+# インデント内部の空行は1行
+
